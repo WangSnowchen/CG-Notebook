ec5722590395658e5181fbbbcb6c870ae9ab44a6
Xue_Wen <62184149+WangSnowchen@users.noreply.github.com>
2024-01-26 11:11:43 +0800

    node

---
+++ Gaffer/Script/NodeDescription.py
@@ -0,0 +1,12 @@
+#
+root = plug.node().scriptNode()
+import os
+folder_path = '/mnt/proj/pj2023dy001/sequences/s9010/s9010_0030/lighting/review'
+def get_latest_image(folder):
+    images = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith((".png", ".jpg", ".jpeg"))]
+    latest_image = max(images, key=os.path.getctime)
+    return latest_image
+    
+latest_image_path = get_latest_image(folder_path)
+a = (f'<img src="{latest_image_path}" alt="" width="300">')
+Gaffer.Metadata.registerValue( root['Node'], 'description', a )
