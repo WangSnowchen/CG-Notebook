23e0110236bdf78015b26decce2d98b753a6f288
Xue_Wen <62184149+WangSnowchen@users.noreply.github.com>
2024-06-19 23:07:35 +0800

    change

--- Nuke/python/change_left&right_%V.py
+++ Nuke/python/change_left&right_%V.py
@@ -2,7 +2,7 @@ import nuke
 
 # 获取所有 Read 节点
 read_nodes = [node for node in nuke.allNodes() if node.Class() == 'Read']
-#print(read_nodes)
+
 # 遍历所有 Read 节点
 for node in read_nodes:
     file_path = node['file'].value()
