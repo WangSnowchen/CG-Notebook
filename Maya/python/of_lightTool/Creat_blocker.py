5b901167ed4ccf843ce85f390fa6481862d31ce6
Xue_Wen <62184149+WangSnowchen@users.noreply.github.com>
2024-07-26 22:09:22 +0800

    change

--- Maya/python/of_lightTool/Creat_blocker.py
+++ Maya/python/of_lightTool/Creat_blocker.py
@@ -1,12 +1,39 @@
-
 import maya.cmds as cmds
 
+light_type_list = ['directionalLight','spotLight','pointLight', 'areaLight','aiAreaLight','aiSkyDomeLight','aiMeshLight','aiPhotometricLight','aiLightPortal']
+light_filter = ['aiBarndoor','aiGobo','aiLightBlocker','aiLightDecay']
 
-def blocker_create(self):
-    light = cmds.ls(sl=1)
-    a = cmds.createNode('aiLightBlocker')
-    cmds.setAttr('%s.density' % a, 1)
-    cmds.setAttr('%s.geometryType' % a, 1)
-    blk = cmds.listTransforms(a)
+def blocker_create():
+    blocker = cmds.createNode('aiLightBlocker')
+    cmds.setAttr(f'{blocker}.density', 1)
+    cmds.setAttr(f'{blocker}.geometryType', 1)
 
+def blocker_connection():
+    selected_all = cmds.listRelatives(cmds.ls(selection=True))
 
+    light_list = []
+    light_filters_list = []
+
+    for i in selected_all:
+        if cmds.nodeType(str(i)) in light_type_list:
+            light_list.append(i)
+        elif cmds.nodeType(str(i)) in light_filter:
+            light_filters_list.append(i)
+    
+    filter_index = len(light_filters_list) + 1
+
+    for light in light_list:
+        light_filters_index = cmds.getAttr(light + '.aiFilters',mi=1)
+        light_filters_index = light_filters_index[-1] + 1
+        #return light_filters_index
+    r = range(light_filters_index,filter_index)
+    iterator = iter(r)
+
+    while True:
+        try:
+            i = next(iterator)
+            for light in light_list:
+                for filter in light_filters_list:
+                    cmds.connectAttr(f'{filter}.message', f'{light}.aiFilters[{i}]')
+        except StopIteration:
+            break
---
+++ Maya/python/of_lightTool/via.py
@@ -0,0 +1,4 @@
+light_type_list = ['directionalLight','spotLight','pointLight', 'areaLight','aiAreaLight','aiSkyDomeLight','aiMeshLight','aiPhotometricLight','aiLightPortal']
+light_filter = ['aiBarndoor','aiGobo','aiLightBlocker','aiLightDecay']
+aov_list = ['AO', 'N', 'P', 'Z','Rim','UV','coat_direct', 'coat_indirect','diffuse_direct','diffuse_indirect','diffuse_albedo','sss_direct','sss_indirect','specular_direct','specular_indirect','transmission_direct', 'transmission_indirect','emission', 'shadow_matte','indirect']
+
