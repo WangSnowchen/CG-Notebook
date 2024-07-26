9f7b83a46d2cee2fb9d550a804d9371415b96d84
Xue_Wen <62184149+WangSnowchen@users.noreply.github.com>
2024-07-26 23:05:58 +0800

    pull

--- Maya/python/of_lightTool/Creat_blocker.py
+++ Maya/python/of_lightTool/Creat_blocker.py
@@ -1,39 +1,34 @@
 import maya.cmds as cmds
 
-light_type_list = ['directionalLight','spotLight','pointLight', 'areaLight','aiAreaLight','aiSkyDomeLight','aiMeshLight','aiPhotometricLight','aiLightPortal']
-light_filter = ['aiBarndoor','aiGobo','aiLightBlocker','aiLightDecay']
+light_type_list = ['directionalLight', 'spotLight', 'pointLight', 'areaLight', 'aiAreaLight', 'aiSkyDomeLight', 'aiMeshLight', 'aiPhotometricLight', 'aiLightPortal']
+light_filter = ['aiBarndoor', 'aiGobo', 'aiLightBlocker', 'aiLightDecay']
 
 def blocker_create():
-    blocker = cmds.createNode('aiLightBlocker')
-    cmds.setAttr(f'{blocker}.density', 1)
-    cmds.setAttr(f'{blocker}.geometryType', 1)
+    try:
+        blocker = cmds.createNode('aiLightBlocker')
+        cmds.setAttr(f'{blocker}.density', 1)
+        cmds.setAttr(f'{blocker}.geometryType', 1)
+    except Exception as e:
+        print(f"Error creating blocker: {e}")
 
 def blocker_connection():
-    selected_all = cmds.listRelatives(cmds.ls(selection=True))
+    try:
+        selected = cmds.ls(selection=True)
+        if not selected:
+            print("No objects selected.")
+            return
+        
+        selected_all = cmds.listRelatives(selected, fullPath=True) or []
 
-    light_list = []
-    light_filters_list = []
+        light_list = [node for node in selected_all if cmds.nodeType(node) in light_type_list]
+        light_filters_list = [node for node in selected_all if cmds.nodeType(node) in light_filter]
 
-    for i in selected_all:
-        if cmds.nodeType(str(i)) in light_type_list:
-            light_list.append(i)
-        elif cmds.nodeType(str(i)) in light_filter:
-            light_filters_list.append(i)
-    
-    filter_index = len(light_filters_list) + 1
+        for light in light_list:
+            light_filters_index = cmds.getAttr(f'{light}.aiFilters', mi=True) or []
+            next_index = max(light_filters_index, default=-1) + 1
 
-    for light in light_list:
-        light_filters_index = cmds.getAttr(light + '.aiFilters',mi=1)
-        light_filters_index = light_filters_index[-1] + 1
-        #return light_filters_index
-    r = range(light_filters_index,filter_index)
-    iterator = iter(r)
-
-    while True:
-        try:
-            i = next(iterator)
-            for light in light_list:
-                for filter in light_filters_list:
-                    cmds.connectAttr(f'{filter}.message', f'{light}.aiFilters[{i}]')
-        except StopIteration:
-            break
+            for filter in light_filters_list:
+                cmds.connectAttr(f'{filter}.message', f'{light}.aiFilters[{next_index}]')
+                next_index += 1
+    except Exception as e:
+        print(f"Error connecting blockers: {e}")
