3604fba8265db4dfad61ae6a1e46e8cdfc951866
Xue_Wen <62184149+WangSnowchen@users.noreply.github.com>
2024-07-26 14:01:51 +0800

    pull

--- Maya/python/of_lightTool/Creat_lightGroup.py
+++ Maya/python/of_lightTool/Creat_lightGroup.py
@@ -1,22 +1,22 @@
 import maya.cmds as cmds
 import mtoa.aovs as aovs
 
-def connect_light_to_aov():
+def connect_lightGroup_to_aov():
     selected_lights = cmds.ls(selection=True)
     if not selected_lights:
         cmds.warning("请先选择灯光对象！")
         return
 
-    light_group_list = []
+    light_group_name_list = []
 
     try:
         # 获取每个灯光对象的aiAov属性作为AOV名称
         for light in selected_lights:
             aov_name = cmds.getAttr(light + '.aiAov')
-            light_group_list.append(aov_name)
+            light_group_name_list.append(aov_name)
 
         # 去除重复的AOV名称
-        lightGroup_aov_list = list(dict.fromkeys(light_group_list))
+        lightGroup_aov_list = list(dict.fromkeys(light_group_name_list))
 
         # 针对每个AOV名称创建对应的AOV
         for name in lightGroup_aov_list:
